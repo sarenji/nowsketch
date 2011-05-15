d763453416f0a387f660884a163681a96260f444
David Peter <david.a.peter@gmail.com>
2011-05-14 22:30:54 -0400

    Sanitized input, added changing nicks support.

--- public/media/js/paint.js
+++ public/media/js/paint.js
@@ -7,10 +7,27 @@ function initNow() {
     // set up sending chat
     $("#msgform").submit(function(e) {
         e.preventDefault();
+        // TODO: Limit messages server/client side.
         var msg = inputMsg.val();
-        now.broadcast(now.room, msg);
+        
+        // attempt to execute commands if message starts with /
+        if (msg[0] == "/") {
+            run_command(msg);
+        } else {
+            now.broadcast(now.room, msg);
+        }
         inputMsg.val('').focus();
     });
+    
+    inputMsg.keydown(function(e) {
+        if (!e.shiftKey && e.keyCode == 13) { // [ENTER]
+            e.preventDefault();
+            $("#msgform").submit();
+        }
+    });
+    
+    // set up brushes
+    context.lineWidth = 2;
     
     // set up drawing
     var oldX = 0;
@@ -55,6 +72,18 @@ function appendMessage(message) {
     }
 }
 
+function run_command(message) {
+    var params = message.replace(/  */, " ").split(" ");
+    
+    switch (params[0]) {
+        case "/nick":
+            now.changeName(params[1]);
+            break;
+        default:
+            now.receiveServerMessage("This command does not exist!");
+    }
+}
+
 /** Nowdraw stuff */
 now.draw = function(oldX, oldY, newX, newY, color) {
     var dx = newX - oldX;
@@ -75,8 +104,8 @@ $(function() {
     displayMsg = $('#messages'); // oh lord, this s thing messed me up.
     
     canvas  = $("#paint");
-    canvas[0].width  = canvas.parent().width();
-    canvas[0].height = canvas.parent().height();
+    canvas[0].width  = 800;
+    canvas[0].height = 600;
     context = canvas[0].getContext('2d');
     
     now.ready(initNow);
--- server.js
+++ server.js
@@ -52,34 +52,56 @@ everyone.on('disconnect', function(clientId) {
     serverMessage(this.now.room, this.now.name + " left #" + this.now.room);
 });
 
+/** Nowchat stuff */
+function sanitize(message) {
+    return message.replace(/&/g, "&amp;")
+                  .replace(/</g, "&lt;")
+                  .replace(/>/g, "&gt;")
+                  .replace(/\n/g, "<br/>");
+}
+
+function serverMessage(room, message) {
+    var room = nowjs.getGroup(room);
+    room.now.receiveServerMessage(sanitize(message));
+}
+
 everyone.now.broadcast = function(room, message) {
     var room = nowjs.getGroup(this.now.room);
 
     // TODO: Check if client sending broadcast is actually in the room!
-    room.now.receiveBroadcast(this.now.name, message);
+    room.now.receiveBroadcast(this.now.name, sanitize(message));
 };
-
-function serverMessage(room, message) {
-    var room = nowjs.getGroup(room);
-    room.now.receiveServerMessage(message);
-}
 
 everyone.now.sendMessage = function(room, message){
     var room = nowjs.getGroup(room);
 
     // clients can't send messages to rooms they are not part of!
     //if (room.hasClient(this.user.clientId)) {
-        this.now.receiveBroadcast(this.now.name, message);
+        this.now.receiveBroadcast(this.now.name, sanitize(message));
     //}
 };
 
-/** Nowsketch stuff */
+var nickValidatorRegex = /^[a-zA-Z0-9]{1,13}$/;
+everyone.now.changeName = function(newNick) {
+    if (nickValidatorRegex.test(newNick)) {
+        // change to just-validated nick
+        var room = nowjs.getGroup(this.now.room);
+        room.now.receiveServerMessage(this.now.name + " is now known as " + newNick);
+        this.now.name = newNick;
+    } else {
+        // send back error message
+        this.now.receiveServerMessage("Keep nicknames to 1-13 alphanumeric characters in length.");
+    }
+}
+
+/** Nowdraw stuff */
 var users = {};
 var drawQueue = {};
 var START_DRAW = 0;
 var KEEP_DRAW  = 1;
 var STOP_DRAW  = 2;
 
+/*
 everyone.now.startDraw = function(roomId, x, y) {
     var room = nowjs.getGroup(roomId);
     var user = this.now.name;
@@ -92,15 +114,6 @@ everyone.now.startDraw = function(roomId, x, y) {
     queue.push({ state: START_DRAW, x : x, y : y });
 };
 
-everyone.now.moveUser = function(x, y) {
-    // TODO: Move user graphic
-};
-
-everyone.now.drawUser = function(oldX, oldY, x, y, color) {
-    var room = nowjs.getGroup(this.now.room);
-    // check if actually in room?
-    room.now.draw(oldX, oldY, x, y, color);
-};
 
 everyone.now.keepDraw = function(roomId, x, y) {
     var room = nowjs.getGroup(roomId);
@@ -124,4 +137,14 @@ everyone.now.stopDraw = function(roomId, x, y) {
     }
     
     queue.push({ state: KEEP_DRAW, x : x, y : y });
+};
+*/
+everyone.now.moveUser = function(x, y) {
+    // TODO: Move user graphic
+};
+
+everyone.now.drawUser = function(oldX, oldY, x, y, color) {
+    var room = nowjs.getGroup(this.now.room);
+    // check if actually in room?
+    room.now.draw(oldX, oldY, x, y, color);
 };
