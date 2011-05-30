a77704c413ae6273dacbbd4f11cc1f54192135ad
David Peter <david.a.peter@gmail.com>
2011-05-30 16:40:38 -0700

    Moved link regex replacement to client from server.

--- public/media/js/paint.js
+++ public/media/js/paint.js
@@ -2,6 +2,31 @@ var canvas, context, dom;
 var drawing = false;
 var started = false;
 
+// domain name list
+var TLDs = [ "aero", "asia", "biz", "cat", "com", "coop", "edu", "gov", "info", "int", "jobs",
+             "mil", "mobi", "museum", "name", "net", "org", "pro", "tel", "travel", "xxx",
+             "ac", "ad", "ae", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "as", "at",
+             "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bm",
+             "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg",
+             "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "cs", "cu", "cv", "cx", "cy", "cz",
+             "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "er", "es", "et", "eu", "fi",
+             "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl",
+             "gm", "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr",
+             "ht", "hu", "id", "ie", "il", "im", "in", "io", "iq", "ir", "is", "it", "je", "jm",
+             "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la",
+             "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me",
+             "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv",
+             "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr",
+             "nu", "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps",
+             "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se",
+             "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "su", "sv", "sy",
+             "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp", "tr",
+             "tt", "tv", "tw", "tz", "ua", "ug", "uk", "us", "uy", "uz", "va", "vc", "ve", "vg",
+             "vi", "vn", "vu", "wf", "ws", "ye", "yt", "za", "zm", "zw" ];
+
+var replaceURLRegex = new RegExp("(https?:\\/\\/)?([-a-zA-Z.]{1,64}\\.(?:"
+              + TLDs.join("|") + ")(?:/[()%/\\w_.~]*)?)(?![0-9A-Za-z])", "i");
+
 function clearSelection() {
     if(document.selection && document.selection.empty) {
         document.selection.empty();
@@ -136,6 +161,12 @@ now.receiveErrorMessage = function(message) {
 function appendMessage(message) {
     var isAtBottom = (dom.displayMsg.attr('scrollTop') == 
         (dom.displayMsg.attr('scrollHeight') - dom.displayMsg.attr('clientHeight')));
+    // turn URLs into real clickable URLs
+    message = message.replace(replaceURLRegex, function() {
+        var url = RegExp.$1 || "http://";
+        url    += RegExp.$2;
+        return '<a href="' + url + '">' + RegExp.$2 + '</a>';
+    });
     // append to the chat window
     dom.displayMsg.append(message);
     if (isAtBottom) {
--- server.js
+++ server.js
@@ -5,30 +5,6 @@ var app     = express.createServer();
 
 var animalNames = fs.readFileSync("animal_list", "UTF-8").split("\n");
 var nickValidatorRegex = /^[a-zA-Z0-9]{1,13}$/;
-// domain name list
-var TLDs = [ "aero", "asia", "biz", "cat", "com", "coop", "edu", "gov", "info", "int", "jobs",
-             "mil", "mobi", "museum", "name", "net", "org", "pro", "tel", "travel", "xxx",
-             "ac", "ad", "ae", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "as", "at",
-             "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bm",
-             "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg",
-             "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "cs", "cu", "cv", "cx", "cy", "cz",
-             "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "er", "es", "et", "eu", "fi",
-             "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl",
-             "gm", "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr",
-             "ht", "hu", "id", "ie", "il", "im", "in", "io", "iq", "ir", "is", "it", "je", "jm",
-             "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la",
-             "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me",
-             "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv",
-             "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr",
-             "nu", "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps",
-             "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se",
-             "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "su", "sv", "sy",
-             "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp", "tr",
-             "tt", "tv", "tw", "tz", "ua", "ug", "uk", "us", "uy", "uz", "va", "vc", "ve", "vg",
-             "vi", "vn", "vu", "wf", "ws", "ye", "yt", "za", "zm", "zw" ];
-
-var replaceURLRegex = new RegExp("(https?:\\/\\/)?([-a-zA-Z.]{1,64}\\.(?:"
-              + TLDs.join("|") + ")(?:/[()%/\\w_.~]*)?)(?!\\S)", "i");
 
 app.configure(function() {
     app.use(express.methodOverride());
@@ -88,12 +64,7 @@ function sanitize(message) {
                   .replace(/</g, "&lt;")
                   .replace(/>/g, "&gt;")
                   .replace(/  /g, " &nbsp;")
-                  .replace(/\n/g, "<br/>")
-                  .replace(replaceURLRegex, function() {
-                      var url = RegExp.$1 || "http://";
-                      url    += RegExp.$2;
-                      return '<a href="' + url + '">' + RegExp.$2 + '</a>';
-                  });
+                  .replace(/\n/g, "<br/>");
 }
 
 function serverMessage(room, message) {
